b9822cddf52c476142ada41b8abafc2d07eba122
niravshah <nirav.shah83@gmail.com>
2015-09-28 20:12:47 +0000

    More Changes

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,14 +1,18 @@
 'use strict';
-
-angular.module('myApp.view1', ['ngRoute'])
-
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
-}])
-
-.controller('View1Ctrl', [function() {
-
-}]);
+angular.module('myApp.view1', ['ngRoute']).config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.when('/view1', {
+      templateUrl: 'view1/view1.html',
+      controller: 'View1Ctrl'
+    });
+  }
+]).controller('View1Ctrl', ['$scope',
+  function($scope) {
+    var profile = localStorage.getItem("ln");
+    if(profile != null) {
+      $scope.ln = profile;
+    }else{
+      $scope.ln = "Couldnt fetch profile form lS";
+    }
+  }
+]);
